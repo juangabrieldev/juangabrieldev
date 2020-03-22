5cfcae9f0a02bf0cb67a9aed4a15c987e6f77968
juangabrieldev <juangabrielpalarpalar@gmail.com>
2020-03-22 21:15:17 +0800

    Add landing page

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,47 @@ import "./index.css";
 
 const IndexPage = () => (
   <div>
+    <main
+      className="
+        bg-white
+        flex
+        flex-col
+        h-screen
+        items-center
+        justify-center
+      "
+    >
+      <h1
+        className="
+          font-semibold
+          sm:text-7xl
+          text-5xl
+          text-gray-900
+        "
+        data-sal="slide-up"
+        data-sal-delay="300"
+        data-sal-duration="600"
+        data-sal-easing="ease"
+      >
+        Hi, I am { " " }
+        <span className="text-purple-500">Gab</span>
+        .
+      </h1>
+      <h3
+        className="
+          font-medium
+          sm:text-2xl
+          text-black
+          text-gray-900
+        "
+        data-sal="slide-up"
+        data-sal-delay="600"
+        data-sal-duration="600"
+        data-sal-easing="ease"
+      >
+        A front-end developer and UI designer
+      </h3>
+    </main>
   </div>
 )
 
