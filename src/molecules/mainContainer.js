b23ca5adc7745ae24cc4291ed0986e5eacd77d93
juangabrieldev <juangabrielpalarpalar@gmail.com>
2020-08-04 01:06:09 +0800

    Create main container

---
+++ src/molecules/mainContainer.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const MainContainer = ({ children }) => (
+    <div>
+        <main className="pl-40 pr-40">
+            { children }
+        </main>
+    </div>
+)
+
+export default MainContainer
