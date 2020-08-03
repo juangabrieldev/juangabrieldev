31776cf49e2d6ad3845571d0f6c4c29e2af76aa6
juangabrieldev <juangabrielpalarpalar@gmail.com>
2020-08-03 22:23:15 +0800

    Add darker shade of purple

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,14 @@
+const { colors } = require("tailwindcss/defaultTheme")
+
 module.exports = {
   theme: {
     extend: {
+      colors: {
+        purple: {
+          ...colors.purple,
+          '1400': '#19122A'
+        }
+      },
       fontSize: {
         '7xl': '5rem'
       }
